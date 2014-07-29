a03d66cef02e20fdb8ecd95ddee382a84d73d210
G A <255sheep@gmail.com>
2014-07-30 01:29:12 +1000

    external random routing; internal shortest-path routing (dijkstra)

--- topolinks.py
+++ topolinks.py
@@ -1,30 +1,44 @@
 import fileinput
 
-fanout = 2
+# user control
+fanout = 4
 
-indent = '        '
-def addLinks():
+indent = '        ' # 8 spaces
+def gotoSubmoduleSection():
+    for line in fileinput.input('tree.ned', inplace=1):
+        print line, # comma somehow excludes \n at the end
+        if 'submodules' in line:
+            break
+
+def addSubmodules():
+    print indent+'beyond: Beyond;'
+    print indent+'core['+str(fanout)+']: Core;'
+    print indent+'pop['+str(fanout**2)+']: PoP;'
+    print indent+'user['+str(fanout**3)+']: User;'
+
+def addConnections():
+    print '    connections:'
+    # beyond-core
     for i in range(1): # just for consistency
         for j in range(i*fanout,(i+1)*fanout):
             print indent+'beyond.gate++ <--> OC48 <--> core['+str(j)+'].gate++;'
+    # core-core (ring)
+    for i in range(fanout-1):
+        print indent+'core['+str(i)+'].gate++ <--> OC12 <--> core['+str(i+1)+'].gate++;'
+    print indent+'core['+str(fanout-1)+'].gate++ <--> OC12 <--> core[0].gate++;'
+    # core-pop
     for i in range(fanout):
         for j in range(i*fanout,(i+1)*fanout):
             print indent+'core['+str(i)+'].gate++ <--> OC3 <--> pop['+str(j)+'].gate++;'
+    # pop-user
     for i in range(fanout*fanout):
         for j in range(i*fanout,(i+1)*fanout):
             print indent+'pop['+str(i)+'].gate++ <--> Cat3 <--> user['+str(j)+'].gate;'
     print '}'
 
-for line in fileinput.input('tree.ned', inplace=1):
-    print line, # comma somehow excludes \n at the end
-    if 'submodules' in line:
-        print indent+'beyond: Beyond;'
-        print indent+'core['+str(fanout)+']: Core;'
-        print indent+'pop['+str(fanout**2)+']: PoP;'
-        print indent+'user['+str(fanout**3)+']: User;'
-        print '    connections:'
-        addLinks()
-        break
+gotoSubmoduleSection()
+addSubmodules()
+addConnections()
 
 #i=1
 #for line in fileinput.input('event_schedule.xml', inplace=1):
--- tree.cc
+++ tree.cc
@@ -2,13 +2,15 @@
 #include <string.h>
 #include <omnetpp.h>
 #include "request_m.h"
+#include "reply_m.h"
 
 class Beyond : public cSimpleModule
 {
 private:
   simsignal_t requestSignal;
-  cTopology topo;
+  //cTopology topo;
   void topoHelper();
+  cPacketQueue *queue;
 protected:
   virtual void initialize();
   virtual void handleMessage(cMessage *msg);
@@ -18,6 +20,7 @@ class Core : public cSimpleModule
 {
 private:
   simsignal_t requestSignal;
+  cPacketQueue *queue;
 protected:
   virtual void initialize();
   virtual void handleMessage(cMessage *msg);
@@ -27,6 +30,7 @@ class PoP : public cSimpleModule
 {
 private:
   simsignal_t requestSignal;
+  cPacketQueue *queue;
 protected:
   virtual void initialize();
   virtual void handleMessage(cMessage *msg);
@@ -40,6 +44,7 @@ private:
   simsignal_t idleSignal;
   cMessage* idleTimer;
   virtual void idle();
+  cPacketQueue *queue;
 protected:
   virtual void initialize();
   virtual void handleMessage(cMessage *msg);
@@ -50,9 +55,34 @@ Define_Module(Core);
 Define_Module(PoP);
 Define_Module(User);
 
+cTopology topo;
+cGate* getNextGate(cSimpleModule* current, Reply* reply) {
+  short userIndex = reply->getDestination();
+//EV << "userindex " << userIndex << endl;
+  char userPath[20];
+  sprintf(userPath, "Tree.user[%d]", userIndex);
+//EV << "userPath " << userPath << endl;
+  cTopology::Node *userNode =
+    topo.getNodeFor(simulation.getModuleByPath(userPath));
+  //EV << "usernode " << userNode << endl;
+  topo.calculateUnweightedSingleShortestPathsTo(userNode);
+  cTopology::Node *currentNode = topo.getNodeFor(current);
+  //EV << "currentnode " << currentNode << endl;
+  cTopology::LinkOut *next = currentNode->getPath(0);
+  //EV << "next " << next << endl;
+  return next->getLocalGate();
+}
+
+bool selectFunction(cModule *mod, void *)
+{
+  // EV << "selfunc debug:" <<  mod->getFullName() << mod->getParentModule() << simulation.getSystemModule() << endl;
+  return mod->getParentModule() == simulation.getSystemModule();
+}
+
 User::~User()
 {
   cancelAndDelete(idleTimer);
+  //delete queue;
 }
 
 void User::initialize()
@@ -60,40 +90,49 @@ void User::initialize()
   idleSignal = registerSignal("idle"); // name assigned to signal ID
   idleTimer = new cMessage("idle timer");
   idle();
+//queue = new cPacketQueue("Packet Queue");
 }
 
 void User::idle()
 {
   simtime_t idleTime = par("idleTime"); // changed via ini
   emit(idleSignal, idleTime);
-  EV << getFullName() <<"idling for " << idleTime << "s\n";
+  EV << getFullName() << " idling for " << idleTime << "s\n";
   scheduleAt(simTime()+idleTime, idleTimer);
 }
 
 void User::handleMessage(cMessage *msg)
 {
   //if msg
-  // assume is always idleTimer for now.
-  // send request
-  Request *req = new Request("request", getIndex()); // use user[] index as message kind
-  //double requestSize = par("requestSize");
-  req->setSize(par("requestSize"));
-  //req->setSize(intuniform(1, 1<<31));
-  EV << "Size: " << req->getSize() << endl;
-  req->setBitLength(1);
-  EV << "BitLength: " << req->getBitLength() << endl;
-  send(req, "gate$o");
-  // idle for a bit
-  idle();
+  if (msg->isSelfMessage()) {
+    // send request
+    Request *req = new Request("request", 123); // use user[] index as message kind
+    //double requestSize = par("requestSize");
+    req->setSize(par("requestSize"));
+    //req->setSize(intuniform(1, 1<<31));
+    EV << "Sending request for " << req->getSize() << " bits\n";
+    req->setSource(getIndex());
+    req->setBitLength(1); // request packet 1 bit long only
+    send(req, "gate$o");
+    // idle for a bit
+    idle();
+  }
+  else { // else received reply
+    Reply* reply = check_and_cast<Reply*>(msg);
+    EV << getFullName() << " received reply of size " << reply->getBitLength() << endl;
+    delete reply;
+  }
 }
 
 void PoP::initialize()
 {
   requestSignal = registerSignal("request"); // name assigned to signal ID
+  //queue = new cPacketQueue("Packet Queue");
 }
 
 void PoP::handleMessage(cMessage *msg)
 {
+  /*
   // logging
   Request *req = check_and_cast<Request*>(msg);
   unsigned int size = req->getSize();
@@ -102,86 +141,132 @@ void PoP::handleMessage(cMessage *msg)
   cChannel *upstream =
     gate("gate$o", 0)->
     getTransmissionChannel();
-  EV << getFullName();
   if (upstream->isBusy()) {
-    // EV << "Busy. Scheduled for " << upstream->getTransmissionFinishTime() << "s\n";
+    EV << "Busy. Scheduled for " << upstream->getTransmissionFinishTime() << "s\n";
     scheduleAt(upstream->getTransmissionFinishTime(), msg);
   }
   else {
     // EV << "Upstream free. Forwarding now.\n";
     send(msg, "gate$o", 0);
+  }
+  */
+  if (msg->getKind() == 123) {
+    // logging
+    Request *req = check_and_cast<Request*>(msg);
+    unsigned int size = req->getSize();
+    emit(requestSignal, size);
+    // forward to first gate (which should be towards the beyond)
+    cChannel *upstream =
+      gate("gate$o", 0)->
+      getTransmissionChannel();
+    if (upstream->isBusy()) {
+      EV << "Busy. Scheduled for " << upstream->getTransmissionFinishTime() << "s\n";
+      scheduleAt(upstream->getTransmissionFinishTime(), msg);
+    }
+    else {
+      // EV << "Upstream free. Forwarding now.\n";
+      send(msg, "gate$o", 0);
+    }
+  }
+  else if (msg->getKind() == 321) {
+    Reply *reply = check_and_cast<Reply*>(msg);
+    cGate* outGate = getNextGate(this, reply);
+    send(reply, getNextGate(this, reply));
+    EV << "got reply. sending out of " << outGate->getFullName() << endl;
   }
 }
 
 void Core::initialize()
 {
   requestSignal = registerSignal("request"); // name assigned to signal ID
+  //queue = new cPacketQueue("Packet Queue");
 }
 
 void Core::handleMessage(cMessage *msg)
 {
-  // logging
-  Request *req = check_and_cast<Request*>(msg);
-  unsigned int size = req->getSize();
-  emit(requestSignal, size);
-  // forward to first gate (which should be towards the beyond)
-  cChannel *upstream =
-    gate("gate$o", 0)->
-    getTransmissionChannel();
-  EV << getFullName();
-  if (upstream->isBusy()) {
-    // EV << "Busy. Scheduled for " << upstream->getTransmissionFinishTime() << "s\n";
-    scheduleAt(upstream->getTransmissionFinishTime(), msg);
+  if (msg->getKind() == 123) {
+    // logging
+    Request *req = check_and_cast<Request*>(msg);
+    unsigned int size = req->getSize();
+    emit(requestSignal, size);
+    // forward to first gate (which should be towards the beyond)
+    cChannel *upstream =
+      gate("gate$o", 0)->
+      getTransmissionChannel();
+    if (upstream->isBusy()) {
+      EV << "Busy. Scheduled for " << upstream->getTransmissionFinishTime() << "s\n";
+      scheduleAt(upstream->getTransmissionFinishTime(), msg);
+    }
+    else {
+      // EV << "Upstream free. Forwarding now.\n";
+      send(msg, "gate$o", 0);
+    }
   }
-  else {
-    // EV << "Upstream free. Forwarding now.\n";
-    send(msg, "gate$o", 0);
+  else if (msg->getKind() == 321) {
+    Reply *reply = check_and_cast<Reply*>(msg);
+    cGate* outGate = getNextGate(this, reply);
+    send(reply, getNextGate(this, reply));
+    EV << "got reply. sending out of " << outGate->getFullName() << endl;
   }
 }
 
 void Beyond::initialize()
 {
-  EV << getFullPath() << endl;
   topoHelper();
+  EV << "gatesize" << gateSize("gate") << endl;
   requestSignal = registerSignal("request"); // name assigned to signal ID
+  //queue = new cPacketQueue("Packet Queue");
 }
 
 void Beyond::handleMessage(cMessage *msg)
 {
   Request *req = check_and_cast<Request*>(msg);
   unsigned int size = req->getSize();
-  EV << "Received request from user " << req->getKind() << " for " << size << "b\n"; // use kind to ID sender
+  int src = req->getSource();
+  EV << "Received request from user " << src << " for " << size << "b\n";
   emit(requestSignal, size);
+
+  // reply via random core
+  Reply *reply = new Reply("reply", 321);
+  reply->setBitLength(size);
+  reply->setDestination(src);
+  send(reply, "gate$o", intuniform(0, gateSize("gate")-1));
 
   // TODO reply with requested size.
   // DONE 1. identify sender. need info in request
   // 2. find route to sender (going down tree so routing needed)
   // 3. queueing messages
-  // 4. add proper queues (cQueue) instead of cycling messages
-  topoHelper();
+  // 3b. add proper queues (cQueue) instead of cycling messages
   delete msg;
 }
 
 void Beyond::topoHelper()
 {
   topo.clear();
+  // 4 methods:
   //topo.extractByModulePath(cStringTokenizer("*").asVector());
-  //topo.extractByNedTypeName(cStringTokenizer("Beyond Core PoP User").asVector());
-  topo.extractByProperty("display");
-  //EV << "topo nodes: " << topo.getNumNodes() << endl;
-for (int i=0; i<topo.getNumNodes(); i++)
-{
-  cTopology::Node *node = topo.getNode(i);
-  ev << "Node i=" << i << " is " << node->getModule()->getFullPath() << endl;
-  ev << " It has " << node->getNumOutLinks() << " conns to other nodes\n";
-  ev << " and " << node->getNumInLinks() << " conns from other nodes\n";
+  topo.extractByNedTypeName(cStringTokenizer("Core PoP User").asVector());
+  //topo.extractByProperty("display");
+  //topo.extractFromNetwork(selectFunction, NULL);
+  EV << topo.getNumNodes() << " nodes in routing topology\n";
+  /*
+  EV << "topo nodes: " << topo.getNumNodes() << endl;
+  for (int i=0; i<topo.getNumNodes(); i++)
+    {
+      cTopology::Node *node = topo.getNode(i);
+      ev << "Node i=" << i << " is " << node->getModule()->getFullPath() << endl;
+      ev << " It has " << node->getNumOutLinks() << " conns to other nodes\n";
+      ev << " and " << node->getNumInLinks() << " conns from other nodes\n";
 
-  ev << " Connections to other modules are:\n";
-  for (int j=0; j<node->getNumOutLinks(); j++)
-  {
-    cTopology::Node *neighbour = node->getLinkOut(j)->getRemoteNode();
-    cGate *gate = node->getLinkOut(j)->getLocalGate();
-    ev << " " << neighbour->getModule()->getFullPath()
-       << " through gate " << gate->getFullName() << endl;
-  }
-}}
+      ev << " Connections to other modules are:\n";
+      for (int j=0; j<node->getNumOutLinks(); j++)
+        {
+          cTopology::Node *neighbour = node->getLinkOut(j)->getRemoteNode();
+          cGate *gate = node->getLinkOut(j)->getLocalGate();
+          ev << " " << neighbour->getModule()->getFullPath()
+             << " through gate " << gate->getFullName() << endl;
+        }
+    }
+  */
+}
+
