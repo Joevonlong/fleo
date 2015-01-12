f5e2ab6fce143e40dde0ac52beac828f58060327
G A <255sheep@gmail.com>
2015-01-12 17:11:19 +1100

    comments

--- cpp/path.h
+++ cpp/path.h
@@ -6,6 +6,12 @@ struct Flow {
     Path path;
     // remember gates too?
     double bps;
+    // TODO int priority;
+    // MAYBE cMessage* completionTimer;
+    // MAYBE uint64_t videoSize/videoLength;
+    // MAYBE uint64_t transferred/completed;
+    // MAYBE simtime_t lastTransferredUpdateTime; //(currentTime-lastupdateTime)*bitrate = additional transferred bits
+    // not necessary? since higher qualities simulated using separate flows so each flow has a fixed bitrate (though aggregate varies)
 };
 
 extern void printPath(Path path);
