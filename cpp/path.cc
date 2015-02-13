3f173494e6c63c58958366acda8fe43af5d39689
G A <255sheep@gmail.com>
2015-02-14 00:17:10 +1100

    made detour finder FIFO to have shorter paths first

--- cpp/path.cc
+++ cpp/path.cc
@@ -1,5 +1,5 @@
 #include <algorithm> // for std::find
-#include <deque>
+#include <queue>
 #include "path.h"
 #include "routing.h"
 #include "flowchannel.h"
@@ -72,57 +72,59 @@ Path getShortestPathBfs(cModule *srcMod, cModule *dstMod) {
     return Path();
 }
 
-//~ Node* nodeCmpTo;
-//~ bool _nodeCmp(Node* n) {
-    //~ return n->getModuleId() == nodeCmpTo->getModuleId();
-//~ }
 bool _hasLoop(Path p) {
     return std::set<Node*>(p.begin(), p.end()).size() != p.size();
 }
 PathList getPathsAroundShortest(cModule *srcMod, cModule *dstMod) {
-    std::set<Path> searchAround, detours;
-    std::set<Path>::size_type prevSize = detours.size();
-    //Node* detour;
-    searchAround.insert(getShortestPathBfs(srcMod, dstMod)); // size is now 1
-    //std::set<Node*> inAPath(detours[0].begin(), detours[0].end()); // ???
-    while (searchAround.size() != 0 /*&& (detours.size() < 100)*/) { // until no new paths are found
-        //prevSize = detours.size();
-        std::set<Path>::iterator path_it = searchAround.begin(); { // for each known path,
-            for (Path::const_iterator branch_it = path_it->begin(); branch_it != path_it->end()-1; ++branch_it) { // for each of its nodes except the last (destinataion),
+    /**
+     * Starts with shortest path, then detours in a FIFO order.
+     * Thus the Paths returned should be some of the shortest.
+     */
+    std::queue<Path> searchingQ;
+    PathList searched;
+    std::set<Path> searchingSet, searchedSet;
+    searchingQ.push(getShortestPathBfs(srcMod, dstMod)); // size is now 1
+    searchingSet.insert(searchingQ.front());
+    //std::set<Node*> inAPath(searched[0].begin(), searched[0].end()); // ???
+    while (searchingQ.size() != 0 /*&& (searched.size() < 100)*/) { // until no new paths are found
+        //prevSize = searched.size();
+        Path path = searchingQ.front(); { // for each known path,
+            for (Path::iterator branch_it = path.begin(); branch_it != path.end()-1; ++branch_it) { // for each of its nodes except the last (destinataion),
                 for (int i = (*branch_it)->getNumOutLinks()-1; i>=0; --i) { // for each neighbour...
                     Node* detour = (*branch_it)->getLinkOut(i)->getRemoteNode();
-                    //nodeCmpTo = detour;
-                    if (std::find(path_it->begin(), path_it->end(), detour) == path_it->end()) { // ... that is not also in the path,
+                    if (std::find(path.begin(), path.end(), detour) == path.end()) { // ... that is not also in the path,
                         for (int j = detour->getNumOutLinks()-1; j>=0; --j) { // for each of that neighbour's neighbours
-                            Path::const_iterator merge_it = std::find(path_it->begin(), path_it->end(), detour->getLinkOut(j)->getRemoteNode()); // look for a node in the aforementioned known path
-                            if (merge_it == path_it->end()) {continue;} // must rejoin path
-                            //nodeCmpTo = detour->getLinkOut(j)->getRemoteNode();
-                            if (std::find(path_it->begin(), branch_it+1, detour->getLinkOut(j)->getRemoteNode()) != branch_it+1) {continue;} // and after branching point
+                            Path::iterator merge_it = std::find(path.begin(), path.end(), detour->getLinkOut(j)->getRemoteNode()); // look for a node in the aforementioned known path
+                            if (merge_it == path.end()) {continue;} // must rejoin path
+                            if (std::find(path.begin(), branch_it+1, detour->getLinkOut(j)->getRemoteNode()) != branch_it+1) {continue;} // and after branching point
                             else { // if so, that forms a new path
-                                Path tmp(path_it->begin(), branch_it); // note: does not include n itself
+                                Path tmp(path.begin(), branch_it); // note: does not include n itself
                                 tmp.push_back(*branch_it); // add node at start of detour
                                 tmp.push_back(detour); // add detour node
-                                tmp.insert(tmp.end(), merge_it, path_it->end()); // add node where detour rejoins path and remainder
-                                if (detours.count(tmp)) {continue;}
-                                if (searchAround.insert(tmp).second) { // but it is not necessarily found uniquely
+                                tmp.insert(tmp.end(), merge_it, path.end()); // add node where detour rejoins path and remainder
+                                if (searchedSet.count(tmp)) {continue;}
+                                if (searchingSet.insert(tmp).second) { // but it is not necessarily found uniquely
                                     if (_hasLoop(tmp)){
-                                        EV << "loop found. based on "; printPath(*path_it);
+                                        EV << "loop found. based on "; printPath(path);
                                         EV << "we get "; printPath(tmp);
                                         cRuntimeError("");
                                     }
+                                    searchingQ.push(tmp);
                                 }
-                                //if (detours.size() > 100) {goto end;}
+                                //~ if (searchedSet.size() +searchAroundSet.size()> 10000) {
+                                    //~ goto end;
+                                //~ }
                             }
                         }
                     }
                 }
             }
         }
-        detours.insert(*path_it);
-        searchAround.erase(path_it);
+        searched.push_back(path); searchedSet.insert(path);
+        searchingQ.pop(); searchingSet.erase(path);
     }
     end:
-    return PathList(detours.begin(), detours.end());
+    return searched;
     /*
      * [insight] if a node has only 2 neighbours, it is traversed only by paths going to the other side
      * []may be optimisations exploiting the fact that input path is shortest
